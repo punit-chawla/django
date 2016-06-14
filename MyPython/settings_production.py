bddab2ccab4cdea5936a4943712aaca5bf5d3575
punit <punit.chawla.wis@gmail.com>
2016-06-14 16:56:34 +0530

    Python - admin

---
+++ MyPython/settings_production.py
@@ -0,0 +1,6 @@
+from settings import *
+
+DEBUG = TEMPLATE_DEBUG = False
+DATABASE_NAME = 'wisdo42j_mypython'
+DATABASE_USER = 'wisdo42j_root'
+DATABASE_PASSWORD = 'welcome2wis'
---
+++ MyPython/urls.py
@@ -0,0 +1,36 @@
+"""MyPython URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls import patterns, include, url
+from login.views import *
+
+urlpatterns = [
+             url(r'^polls/', include('polls.urls')),
+             url(r'^students/', include('students.urls')),
+             url(r'^teachers/', include('teachers.urls')),
+             url(r'^user/', include('login.urls')),
+             url(r'^classes/', include('manage_classes.urls')),
+             url(r'^admin/', admin.site.urls),
+              url(r'^$',home),
+              url(r'^logout/$', logout_page),
+              url(r'^login/$',  'django.contrib.auth.views.login'),
+              url(r'^accounts/login/$',
+                  'django.contrib.auth.views.login'),  # If user is not login it will redirect to login page
+              url(r'^register/$', register),
+              url(r'^register/success/$', register_success),
+              url(r'^home/$', home),
+]
